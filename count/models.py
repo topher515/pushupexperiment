0e76b05d1fd4fae29974e16c87f3ffcee6ffa1d1
Chris Wilcox <topher515@gmail.com>
2011-02-04 11:26:59 -0800

    Display how many for a set of 4

--- count/models.py
+++ count/models.py
@@ -14,6 +14,19 @@ class Workout(models.Model):
 	
 	def for_today(self):
 		return days_ago(self.start_date) + self.start_number
+		
+	def for_today_sets(self,sets):
+		sets_for_today = []
+		left_todo = self.for_today()
+		each = left_todo / sets
+		leftover = left_todo % sets
+		for i in xrange(sets):
+			sets_for_today.append(each+(1 if leftover > 0 else 0))
+			leftover -= 1
+		return sets_for_today
+		
+	def for_today_sets_4(self):
+		return self.for_today_sets(4)
 
 class Count(models.Model):
 	workout = models.ForeignKey(Workout,unique_for_date="date")
